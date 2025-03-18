3ec52a9ab4641b292f3d7c821bddc161eed4aac8
devRokib <rakibahmedra971162@gmail.com>
2025-03-18 20:09:43 +0600

    practice some variable . asign and hoisting

--- 06Variable/variable.js
+++ 06Variable/variable.js
@@ -1,2 +1,25 @@
 console.log('hello bangladesh')
 console.log('hello var')
+
+    var number ; // declare a store name
+    number = 36 ; // assign store value
+    console.log(number) // console for test output
+
+
+// var x = 5;
+//     x = 7 ;
+//     x = x + x
+//     alert(x)     
+
+
+    var a = 5 ; // declare and asign a value
+    var b = 6 ; // declare and asign a value
+    var c = a + b ; // sum of a nd b 
+    console.log(c) // total 
+
+    var name = "rakib ahmed"; // declare and asign a value
+    console.log(name)
+    
+    age = 29 // first  asign a value
+    var age ; // last declare a store
+    console.log(age) // hoisted this data
