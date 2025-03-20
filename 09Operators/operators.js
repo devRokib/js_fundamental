8c8f21eee9dac092806b861c17fb7722dd9db7d1
devRokib <rakibahmedra971162@gmail.com>
2025-03-20 21:46:12 +0600

    take a basic knowledge about js operator and some practices

---
+++ 09Operators/operators.js
@@ -0,0 +1,92 @@
+
+let x = 10 // assignment operator // assign a value to a variable
+
+let a = 2 // assign a value 2 to a
+let b = 3 // assign value 3 to b
+let c = a + b // assign value a and b to c
+console.log(c)
+
+// The Addition Operator (+) adds numbers:
+
+let num1 = 60 ;
+let num2 = 70 ;
+let sum = num1 + num2 /// addition num1 and num2 to sum
+console.log(sum + " this is addition output")
+
+// the Subtraction operator (-) subtraction number
+
+let num3 = 40 ;
+let num4 = 30 ;
+let sum2 = num3 - num4 ;
+console.log(sum2 + " this is substraction output")
+
+// The Multiplication Operator (*) multiplies numbers:
+
+let num5 = 50 ;
+let num6 = 8 ;
+let sum3 = num5 * num6 ;
+console.log(sum3 + "this is multiplies output")
+
+// the exponentiation operator (**) multiplies numbers
+
+let num7 = 4 ;
+let num8 = 3 ;
+let sum4 = num7 ** num8 ;
+console.log(sum4 + " this is exponentiation output")
+
+
+// Division
+
+let num9 = 8 ;
+let num10 = 3 ;
+let sum5 = num9 / num10 ;
+console.log(sum5 + " this is division output")
+
+// modulus
+
+let sum6 = num9 % num10 ;
+console.log(sum6 + " this is modulus output")
+
+
+let y = 6 ;
+    y += 10 ;
+    console.log(y + " this is y value")
+
+    
+// javascript string operator
+
+let str1 = " love "
+let str2 = " you "
+let result = str1 + str2 ;
+console.log(result)
+
+let str3 = "10" + 30 ;
+console.log( typeof str3)
+
+let str4 = "hello" + 5
+console.log(typeof str4)
+
+let str5 = 8 + 8 ;
+console.log(typeof str5)
+
+let str6 = 5 + "2" ;
+console.log(str6)
+
+let text = "hello bangladesh " ;
+    text += "how are you"
+    console.log(text)
+
+    let exprassion = (100 + 50) * 2;
+    console.log(exprassion)
+
+//increment
+
+    let inc = 5
+    inc ++
+    console.log(inc)
+
+    // decrement
+
+    let dec = 9 ;
+    dec -- 
+    console.log(dec)
