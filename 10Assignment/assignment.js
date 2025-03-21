b650400444e8943c2f1172f26cc2a169a5a5bccd
devRokib <rakibahmedra971162@gmail.com>
2025-03-21 18:44:39 +0600

    in this folder i practices assignment operator

---
+++ 10Assignment/assignment.js
@@ -0,0 +1,50 @@
+{
+    let x = 10
+        x += 5 // x = x + 5
+        console.log(x +" this is addition value")
+
+}
+
+{
+    let text = "hello assignment"
+        text += " operator" // text = text + "operator"
+        console.log(text +" this is addition value")
+}
+
+{
+    let x = 10 ;
+        x -= 5;
+        console.log(x +" this is substraction value")
+}
+
+{
+    let x = 10 ;
+         x *= 5 ;
+         console.log(x +" this is multiplies value")
+}
+
+{
+    let x = 10 ;
+        x /= 5 ;
+        console.log(x +" this is devide value")
+}
+
+{
+    let x = 10 ;
+        x %= 3 ;
+        console.log(x +" this is remainder value")
+}
+
+{
+    let x = 10 ;
+        x **= 5 ;
+        console.log(x +" this is exponentiation value")
+}
+
+// left shift operator
+
+{
+    let x = -100;
+        x <<= 5 ;
+        console.log(x +" this is left shift value")
+}
