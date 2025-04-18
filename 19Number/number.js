c777a0531d0dafe089f9d86a498840bfc38e9304
devRokib <rakibahmedra971162@gmail.com>
2025-04-18 18:22:48 +0600

    there are some practices about number in js

--- 19Number/number.js
+++ 19Number/number.js
@@ -7,3 +7,16 @@ let num3 = 520 ;
 console.log(num3)
 let num4 = 12 ;
 console.log(num4)
+
+
+
+const nmbr = 4 // a number without decimel
+const nmbr2 = 3.34 // a number with decimel
+
+console.log(nmbr)
+console.log(nmbr2)
+
+let xy = 123e5; 
+const nmbr4 = 123e-5
+console.log(xy)
+console.log(nmbr4)
