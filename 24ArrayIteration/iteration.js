6eb44f691987a888030ab2f0f7ccc4360339d03d
devRokib <rakibahmedra971162@gmail.com>
2025-04-24 21:41:22 +0600

    array iteration practices

---
+++ 24ArrayIteration/iteration.js
@@ -0,0 +1,9 @@
+const numbers = [25,8,35, 12, 18,23]
+function myFunction(value ,index, array){
+    console.log(value)
+    console.log(index)
+    console.log(array)
+    console.log("---------")
+}
+ numbers.forEach(myFunction)
+
