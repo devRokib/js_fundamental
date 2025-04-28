d5a0bf80b7b31cc94c1c79573bf0f0b070b5932b
devRokib <rakibahmedra971162@gmail.com>
2025-04-28 20:26:27 +0600

    practicing problem solved

--- 31ProvlemSolving/problem.js
+++ 31ProvlemSolving/problem.js
@@ -16,5 +16,67 @@ function findRoll (a,b){
 let findSerial = Roll.sort(findRoll)
 console.log(findSerial)
 
+function isLeapYear (year){
+    if( ( year % 400 === 0 ) || ((year % 4 === 0 && year % 100 !== 0)) ){
+        console.log(`${year} is leap year`)
+    } else{
+        console.log(` ${year} is not leap year `)
+    }
+}
+
+isLeapYear(2028)
+
+const vowels = ['a','e','i','o','u','A','E','I','O','U']
+
+function countVowels (sentence){
+    let count = 0 ;
+    const letter = Array.from(sentence)
+    letter.forEach( function (value) {
+        if ( vowels.includes(value))
+            count ++
+    })
+
+    return count
+}
+const totalVowels = countVowels('problem solving with count Vowels')
+console.log(totalVowels)
 
 
+const numberArr = [2,3,5,6, 8, 9, 20,35,16]
+
+function findEvenNumber (number){
+    let countNumber = 0
+    number.forEach(function (num){
+        if (num % 2 === 0){
+            countNumber ++
+        }
+    })
+    
+    return countNumber
+}
+
+const totalEvenNumber = findEvenNumber(numberArr)
+console.log(totalEvenNumber)
+
+
+const mixNumber = [4,5,21,53, 64,73,52,65,72,77]
+
+function oddNumber (oddNum){
+    let countOdd = 0
+    oddNum.forEach( function (odd){
+        if ( odd % 2 === 1){
+            countOdd ++
+        }
+    })
+    return countOdd
+}
+
+let totalOddNumber = oddNumber(mixNumber)
+console.log(totalOddNumber)
+
+const numbers = [2,5,6,7,8,9,1,2,5,6,7,3]
+
+let duplicats = numbers.filter( function ( value,index,array ){
+    return array.indexOf(value) !== index
+})
+console.log(duplicats)
