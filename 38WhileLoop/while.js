58694ade4f37d2ea0fb59701b50e2a2c11036bf7
devRokib <rakibahmedra971162@gmail.com>
2025-05-01 11:13:39 +0600

    here i practices while and do while loop

---
+++ 38WhileLoop/while.js
@@ -0,0 +1,18 @@
+let i = 0
+let text = ''
+
+while( i < 10 ){
+   text += " the number is , " + i 
+    i++
+}
+console.log(text)
+
+let j = 0
+let text2 = ''
+
+do{
+    text2 += ' the number is :' + j
+    j++
+} while(j < 10)
+    
+console.log(text2)
