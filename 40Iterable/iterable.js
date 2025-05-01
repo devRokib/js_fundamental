2bcd6e632710be11e254f6e47f6dc0ff4c1466a1
devRokib <rakibahmedra971162@gmail.com>
2025-05-01 15:03:27 +0600

    practices iterables

---
+++ 40Iterable/iterable.js
@@ -0,0 +1,17 @@
+let str = 'test JavaScript'
+
+for( let strIterate of str ){
+    console.log(strIterate)
+}
+
+let arr = [2,3,5,6,7,8,9]
+
+for(let number of arr){
+    console.log(number)
+}
+
+const letters = new Set(['a','b','c','d'])
+
+for(let x of letters){
+    console.log(x)
+}
