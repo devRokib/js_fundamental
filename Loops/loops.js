bcabd1c4f6465f841d1834714721b4a75ecfbf89
devRokib <rakibahmedra971162@gmail.com>
2025-09-14 19:50:23 +0600

    loops practices

---
+++ Loops/loops.js
@@ -0,0 +1,15 @@
+let numbers = ""
+
+for( let i = 0 ; i < 20 ; i++){
+    numbers += " The Number is " + i  + "<br>"
+    
+}
+document.getElementById("demo").innerHTML=numbers
+
+
+
+let some = "" ;
+for(let text = 0 ; text < 40 ; text++){
+    some +=  "The Number is " + text +"<br>"
+}
+document.getElementById('texts').innerHTML= some
