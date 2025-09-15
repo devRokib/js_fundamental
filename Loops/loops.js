e5984eaf8800e9dc1786f56200976ce3470f62d3
devRokib <rakibahmedra971162@gmail.com>
2025-09-15 21:02:42 +0600

    while loop and do while loop practices

--- Loops/loops.js
+++ Loops/loops.js
@@ -12,4 +12,57 @@ let some = "" ;
 for(let text = 0 ; text < 40 ; text++){
     some +=  "The Number is " + text +"<br>"
 }
-document.getElementById('texts').innerHTML= some
+document.getElementById('texts').innerHTML = some
+
+// shile loops ======================================
+// ==================================================
+
+let conuntNumber = ""
+let nmbr = 0
+while ( nmbr < 15){
+    conuntNumber = conuntNumber + 'The number is ' + nmbr + '<br>' ;
+    nmbr++
+}
+document.getElementById('whileLoop').innerHTML = conuntNumber
+
+let countPlusNumber = ""
+let plusNumber = 0
+  
+while( plusNumber < 20){
+    countPlusNumber = countPlusNumber + plusNumber + "<br>" ;
+    plusNumber = plusNumber + 2
+}
+document.getElementById("plusNumbers").innerHTML = countPlusNumber
+
+
+let count = ""
+let countDown = 10
+
+while (countDown > 0){
+    count = count + countDown + "<br>"
+    countDown--
+}
+document.getElementById("countDowns").innerHTML = count
+
+// do while loops =========================================
+// ========================================================
+
+let doWhile = ""
+let num = 0
+do{
+    doWhile = doWhile + num + "<br>"
+    num++
+}
+while(num < 10)
+
+document.getElementById("doWhiles").innerHTML = doWhile
+
+let falsyValue = ""
+let value = 20
+
+do{
+    falsyValue = falsyValue + value + "<br>";
+    value ++
+}while(value < 10)
+
+document.getElementById("falsyDoWhile").innerHTML = falsyValue
